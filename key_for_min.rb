646f3c53a3ecd77e0d038246cf7cb85a54661e53
dwane richards <dwanerich@gmail.com>
2019-12-20 00:23:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(ikea)
-  ikea.each.with_index do |part, index|
-    index
-  end
+  
     val1 = nil
     val2 = nil
-
+  ikea.each.with_index do |part, index|
+    val2 < index
+  end
+    
 end
